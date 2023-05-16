45b335b7152e59c68ccea0d4cfff168eba6fc40c
stubbornmarlin3 <arcarter129@gmail.com>
2023-05-16 11:43:32 -0400

    Testing connection to database with mysql connector.

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from config import HOSTNAME, USER, PASSWORD
+import mysql.connector as mysql
+
+db_connection = mysql.connect(
+    host=HOSTNAME,
+    user=USER,
+    password=PASSWORD
+)
+
+print(db_connection.get_server_info())
