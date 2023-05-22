b9f3ee8e31d6163a7105313e6537f24dc817cf90
stubbornmarlin3 <arcarter129@gmail.com>
2023-05-22 00:41:25 -0400

    testing

--- socketServerTest.py
+++ socketServerTest.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
-import pytermgui
+import socket
+import time
+
+socketClient = socket.socket()
+
+socketClient.connect(("192.168.1.4",35491))
+
+time.sleep(5)
+socketClient.send(b'login\narcar\nstryker0')
+
+data = socketClient.recv(128)
+print(data)
+
+
+socketClient.close()
 
 
 
