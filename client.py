0af3a50bc0ff8580934cc62c6e049a28090710e6
stubbornmarlin3 <arcarter129@gmail.com>
2023-05-24 00:47:46 -0400

    lots of work to login method on client and server

--- client.py
+++ client.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 
-import time
+import rsa
 import socket
 from passlib.hash import pbkdf2_sha256
 from cryptography.fernet import Fernet
 from getpass import getpass
-from threading import Thread
+
+class UserDoesNotExist(Exception):
+    "Raised when the user does not exist in Database"
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class IncorrectPassword(Exception):
+    "Raised when the password hash does not match one in Database"
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
 class User:
 
-    def __init__(self) -> None:
+    def __init__(self, username: str, display_name: str, password: str, public_key: rsa.PublicKey, private_key: rsa.PrivateKey) -> None:
 
-        self.username = None
-        self.display_name = None
-        self._password = None
-        self._password_hash = None
-        self.public_key = None
-        self.private_key = None
+        self.username = username
+        self.display_name = display_name
+        self.password = password
+        self.public_key = public_key
+        self.private_key = private_key
 
 
 class Client:
@@ -29,17 +39,17 @@ class Client:
 
         # Create a socket for the client to use
         self.socketClient = socket.socket()
-        self.socketClient.settimeout(30)
+        self.socketClient.settimeout(20)
 
 
-    def send_command(self, command: str) -> str:
+    def send_command(self, command: str) -> list[str]:
         # Used to send command to the server
 
         # Send command
         self.socketClient.send(command.encode())
 
-        # Get return from server
-        return self.socketClient.recv(2048).decode()
+        # Get return from server as a list of strings seperated by '||' as defined by server
+        return self.socketClient.recv(2048).decode().split("||")
 
     def login(self) -> User:
 
@@ -56,15 +66,54 @@ class Client:
 
             # Send the login command w/ parameter "username" to the server
             # Store return in result - should be old_salt and new_salt
-            result = self.send_command(f"login {username}").split()
+            # If user doesn't exist, will return nothing
+
+            result = self.send_command(f"login {username}")
+
+            if not result[0]:
+                raise UserDoesNotExist
             
             old_salt = result[0].encode()
-            new_salt = result[1].encode()
+            session_salt = result[1].encode()
+
+            # Generate the new_hash from the new salt and old hash
 
             old_hash = pbkdf2_sha256.hash(password, salt=old_salt)
-            new_hash = pbkdf2_sha256.hash(old_hash, salt=new_salt)
+            test_hash = pbkdf2_sha256.hash(old_hash, salt=session_salt)
 
-            result = self.send_command(new_hash)
+            # Send the test_hash to the server
+            # Result should be the user data returned: display_name, public_key, enc_private_key respectively
+            # If password is wrong, will return nothing
+
+            result = self.send_command(test_hash)
+
+            if not result[0]:
+                raise IncorrectPassword
+
+            display_name = result[0]
+            public_key = result[1]
+            enc_private_key = result[2]
+
+            # Unencrypt the private key that was returned from the server
+
+            fernet_key_old = pbkdf2_sha256.hash(password, salt=old_hash.encode())
+            fernet_key_old = bytes(f"{fernet_key_old[-43:]}=".replace(".","+"),"utf-8")
+
+            f_old = Fernet(fernet_key_old)
+
+            private_key = f_old.decrypt(enc_private_key).decode()
+
+            # Make public_key and private_key into rsa types for the User class
+            
+            public_key = public_key.split("$")
+            private_key = private_key.split("$")
+
+            public_key_rsa = rsa.PublicKey(public_key[0], public_key[1])
+            private_key_rsa = rsa.PrivateKey(public_key[0], public_key[1], private_key[0], private_key[1], private_key[2])
+
+            return User(username, display_name, password, public_key_rsa, private_key_rsa)
+
+            
             
 
         except ConnectionRefusedError: # Usually because not connected to internet (or the server is down)
@@ -73,6 +122,11 @@ class Client:
         except TimeoutError: # Server took too long to respond
             print("TimeoutError")
 
+        except UserDoesNotExist: # User did not exist
+            print("UserDoesNotExist")
+
+        except IncorrectPassword: # Password was incorrect
+            print("IncorrectPassword")
 
     def create_account(self) -> None:
         pass
@@ -83,3 +137,4 @@ if __name__ == "__main__":
 
     c1 = Client("localhost", 35491)
     u1 = c1.login()
+    print(u1.private_key)
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import time
 import os
 import socket
 import mysql.connector as mysql
+from datetime import datetime
 from passlib.hash import pbkdf2_sha256
 from threading import Thread
 from credentials import mysql_config
@@ -17,33 +17,52 @@ class Server:
         self.server_port = server_port
 
         # Create a socket for the client to use
+        print(f"{datetime.now()} Creating server socket...")
         self.socketServer = socket.socket()
-        print("Server socket created")
+        self.connections = []
 
         self.socketServer.bind((self.server_hostname, self.server_port))
         self.socketServer.listen()
+        print(f"{datetime.now()} Server socket created and listening on {self.server_hostname}:{self.server_port}!")
+        
 
     def __del__(self) -> None:
 
         # Used to cleanup the socket server after the program exits
+        print(f"{datetime.now()} Closing active connections...")
+        for connection in self.connections:
+            connection.close()
+
         self.socketServer.close()
-        print("Server socket closed")
+        print(f"{datetime.now()} Server socket closed!")
 
 
     def start_connection_manager(self) -> None:
+        print(f"{datetime.now()} Started connection manager!")
 
-        # The main loop for the server
-        while True:
+        try:
+            # The main loop for the server
+            while True:
 
-            connection, addr = self.socketServer.accept()
-            print("Connection established!")
-            Thread(target=self.recv_command, args=[connection]).start()
-            print("Thread made!")
+                print(f"{datetime.now()} (ConnectionManager) Waiting for new connection...")
+                connection, addr = self.socketServer.accept()
+                self.connections.append(connection)
+                print(f"{datetime.now()} (ConnectionManager) Connection established to {connection.getpeername()}!")
+                Thread(target=self.recv_command, args=[connection]).start()
+                print(f"{datetime.now()} (ConnectionManager) Thread created!")
+
+        except KeyboardInterrupt:
+                print(f"{datetime.now()} KeyboardInterrupt to stop server...")
+
+        print(f"{datetime.now()} (ConnectionManager) Stopped connection manager!")
+
 
     def recv_command(self, connection: socket.socket) -> str:
+        print(f"{datetime.now()} {connection.getpeername()} Waiting for command...")
 
         command = connection.recv(128).decode().split()
-        print(command)
+
+        print(f"{datetime.now()} {connection.getpeername()} Received command {command}!")
 
         match command[0]:
             case "login":
@@ -51,9 +70,14 @@ class Server:
             case "create":
                 self.create_account(command[1], connection)
             case other:
+                print(f"{datetime.now()} {connection.getpeername()} Unknown command!")
                 connection.send(f"Unknown command {command}".encode())
 
+        print(f"{datetime.now()} {connection.getpeername()} Closing connection...")
+        self.connections.remove(connection)
         connection.close()
+        print(f"{datetime.now()} (ConnectionManager) Connection closed!")
+
 
     def send_data(self, data: str, connection: socket.socket) -> str:
         # Send data
@@ -63,16 +87,21 @@ class Server:
         return connection.recv(2048).decode()
 
     def login(self, username: str, connection: socket.socket) -> None:
+        print(f"{datetime.now()} {connection.getpeername()} Starting login...")
+
 
         # Create connection to mysql
         with mysql.connect(**mysql_config) as db:
             
-            print("Connected to mysql database")
+            print(f"{datetime.now()} {connection.getpeername()} Connected to MySQL Database!")
             
             # Create cursor for mysql
             with db.cursor() as dbc:
 
-                print("Create cursor")
+                print(f"{datetime.now()} {connection.getpeername()} Created MySQLCursor!")
+
+                # Get the password_hash and password_salt from the Database to compare to client_hash
+                # Also used to confirm the users exists, otherwise stop login
 
                 dbc.execute(
                     "SELECT password_hash, password_salt FROM users WHERE username = %s LIMIT 1;",
@@ -80,25 +109,45 @@ class Server:
                 )
                 result = dbc.fetchall()
 
-                if not result:
-                    print("User does not exist")
+                if not result: # No user
+                    print(f"{datetime.now()} {connection.getpeername()} Invalid username given: '{username}'!")
                     return
                 
-                old_hash = result[0][0] # Get the hash returned from the database
-                old_salt = result[0][1] # Get the salt returned from the database
-                new_salt = b64encode(os.urandom(64)) # Generate a new salt
+                result = result[0] # Since it is a list of tuples and we only have 1 tuple, drop the list
                 
-                client_hash = self.send_data(f"{old_salt} {new_salt.decode()}", connection)
+                old_hash = result[0] # Get the hash returned from the database
+                old_salt = result[1] # Get the salt returned from the database
+                session_salt = b64encode(os.urandom(64)) # Generate a new salt
+                
+                # Send back the old and new salt to client. The client will return the hash to check
+                client_hash = self.send_data(f"{old_salt}||{session_salt.decode()}", connection)
+                print(f"{datetime.now()} {connection.getpeername()} Username: {username}")
 
-                if pbkdf2_sha256.hash(old_hash, salt=new_salt) != client_hash:
-                    print("Incorrect password")
+                if pbkdf2_sha256.hash(old_hash, salt=session_salt) != client_hash: # Compare hashes
+                    print(f"{datetime.now()} {connection.getpeername()} Incorrect password given for username '{username}'!")
                     return
                 
+                print(f"{datetime.now()} {connection.getpeername()} Correct password given!")
 
+                
+                # If the password hashes match, get display name and keys from the Database to give client user data
+                # (Although someone could just hack the Database to get this information, 
+                # the only thing that is sacred is the private_key, which is encrypted by the password.)
+                # This means that the only way to decrypt the key is with the password, which is never stored on the server or database
 
+                dbc.execute(
+                    "SELECT display_name, public_key, enc_private_key FROM users WHERE username = %s LIMIT 1;",
+                    (username,)
+                )
+                result = dbc.fetchall()[0] # Drop the list, should always return something since user is confirmed to be there
 
+                display_name = result[0]
+                public_key = result[1]
+                enc_private_key = result[2]
 
+                print(f"{datetime.now()} {connection.getpeername()} Sending {username}'s account data...")
 
+                self.send_data(f"{display_name}||{public_key}||{enc_private_key}", connection)
 
 
     def create_account(self, username: str) -> None:
